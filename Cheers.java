97aaa4ae46a8ace7b23244f9f5ce8aff695888ef
Ofek Mossinson <ofekmoss@Ofeks-MacBook-Pro-2.local>
2025-11-07 12:20:41 +0200

    Question 3

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,17 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String name = args[0];
+                String vowels = "AEFHILMNORSX";
+
+                for (char c : name.toCharArray()) {
+                        System.out.println(String.format("Give me a %s: %s!", (vowels.indexOf(c) != -1 ? "an" : "a"), Character.toUpperCase(c)));
+                }
+
+                int numberOfCheers = Integer.parseInt(args[1]);
+                System.out.println("What does that spell?");
+                for (int i = 0; i < numberOfCheers; i++) {
+                        System.out.println(String.format("%s!!!", args[0].toUpperCase()));
+                }
         }
 }
