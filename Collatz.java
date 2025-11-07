70e2c2f19a0888aedfb05ad1b68a941b9573e6cd
Ofek Mossinson <ofekmoss@Ofeks-MacBook-Pro-2.local>
2025-11-07 13:02:44 +0200

    Question 5

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,32 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
+	
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		Boolean isV = args[1].equals("v");
+	    for (int i = 1; i <= Integer.parseInt(args[0]); i++) {
+			int n = i;
+			int count = 2;
+			if (isV) System.out.print(String.format("%s ", n));
+			n = nextCollatz(n);
+			while (n != 1) {
+				if (isV) System.out.print(String.format("%s ", n));
+				n = nextCollatz(n);
+				count++;
+			}
+			if (isV) System.out.print(String.format("%s ", n));
+			if (isV) System.out.println(String.format("(%s)", count));
+		}
+		if (isV) System.out.println();
+		System.out.println(String.format("Every one of the first %s hailstone sequences reached 1.", args[0]));
 	}
+	
+	public static int nextCollatz(int n) {
+		if (n % 2 == 0) {
+			n = n / 2;
+		} else {
+			n = 3 * n + 1;
+		}
+		return n;
+	}
+
 }
